5d52690bb1542d6e389125eba49d268d0015aae4
Jorge FdCD <cossio@users.noreply.github.com>
2024-05-19 14:27:23 +0200

    pluto

---
+++ pluto/2024-05-19 Comparison to IndepSite.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f00c1a4f-51fc-4c0e-bbd1-b201691f8642
+import Pkg, Revise; Pkg.activate(Base.current_project())
+
+# ╔═╡ fef2bcf6-2aa7-463c-a194-3f2841a1395b
+md"# Imports"
+
+# ╔═╡ 9bcddb57-9009-4594-ba05-85b856893741
+import Ab4Paper2023
+
+# ╔═╡ c37b2ac5-8d71-423d-805d-cc747a72c6c2
+import Makie, CairoMakie
+
+# ╔═╡ a48fbc48-7b4a-46cc-b0d5-703bd713ee94
+md"# Scripts"
+
+# ╔═╡ c121a08c-7592-4955-8014-33cbb86629d3
+
+
+# ╔═╡ Cell order:
+# ╠═fef2bcf6-2aa7-463c-a194-3f2841a1395b
+# ╠═f00c1a4f-51fc-4c0e-bbd1-b201691f8642
+# ╠═9bcddb57-9009-4594-ba05-85b856893741
+# ╠═c37b2ac5-8d71-423d-805d-cc747a72c6c2
+# ╠═a48fbc48-7b4a-46cc-b0d5-703bd713ee94
+# ╠═c121a08c-7592-4955-8014-33cbb86629d3
